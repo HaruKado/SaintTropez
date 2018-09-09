1e65d850512cc8b4f8477b37e85acb77f2b31148
門平晴樹 <kadohiraharuki@wlan-napt-001.u-aizu.ac.jp>
2018-09-09 19:37:57 +0900

    4つのオプション機能追加

--- app/src/main/java/com/example/kadohiraharuki/sainttropez/MainActivity.kt
+++ app/src/main/java/com/example/kadohiraharuki/sainttropez/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.kadohiraharuki.sainttropez
 
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import com.example.kadohiraharuki.sainttropez.R.id.imageView
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,6 +16,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //ここでは画像を表示しているImageViewを登録
+        registerForContextMenu(imageView)
     }
 
     //onCreateOptionsMenuをオーバーライドしてメニューの初期化
@@ -54,5 +60,80 @@ class MainActivity : AppCompatActivity() {
         //親クラスの同名メソッドを実行して値を返しているが、これはAPIの決まり
         //when式で指定している以外のIDが取得された場合に何も返されなくなるのを避けるための記述
         return super.onOptionsItemSelected(item)
+    }
+
+    //コンテキストメニューとして登録されたビューを長押しすると、onCreateContextMenuメソッドが呼ばれるので、それをオーバーライド
+    override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        //menuInflaterプロパティでMenuInflaterクラスのインスタンスを取得、infrateメソッドによりメニューXMLファイルをコンテキストに設定
+        menuInflater.inflate(R.menu.context, menu)
+    }
+
+    //onContextItemSelectedメソッドでオプションメニューのメニュー項目がタップされた時に呼ばれる
+    override fun onContextItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId) {
+            //メニュー処理を続行するにはfalse、終了するにはtrueを返す
+            R.id.sms -> {
+                //データを渡すために、電話番号からUriオブジェクトを生成
+                val number = "999-9999-9999"
+                //parseメソッドでuriオブジェクト生成
+                val uri = Uri.parse("sms:$number")
+                //アクションとしてIntent.ACTION_VIEWを指定してインテントを作成
+                var intent = Intent(Intent.ACTION_VIEW)
+                //作成したURIオブジェクトをデータとして設定
+                intent.data = uri
+                //startActivityにintentを渡して起動
+                startActivity(intent)
+                return true
+            }
+            R.id.mail -> {
+                //メールアプリに渡すアドレス、件名、本文を変数に代入
+                val email: String = "nobady@example.com"
+                val subject: String = "予約問い合わせ"
+                val text: String = "以下の通り予約希望します"
+                //データを渡すuriオブジェクト作成
+                val uri = Uri.parse("maiito")
+                //インテントアクションとしてIntent.ACTION_SENDTO（メッセージ配信機能）を指定して作成
+                val intent = Intent(Intent.ACTION_SENDTO)
+                intent.apply {
+                    data = uri
+                    //arrayof関数により配列にしておく
+                    putExtra(Intent.EXTRA_EMAIL, arrayListOf(email))
+                    putExtra(Intent.EXTRA_SUBJECT, subject)
+                    putExtra(Intent.EXTRA_TEXT, text)
+                }
+                //暗黙インテントを処理できるアプリをユーザがインストールしていない場合
+                //packageManagerはインストールされているアプリに関する情報を保持しているクラス
+                if(intent.resolveActivity(packageManager) != null){
+                    startActivity(intent)
+                }
+                return true
+            }
+            R.id.share -> {
+                val text: String = "美味しいレストランを紹介します"
+                val intent = Intent(Intent.ACTION_SEND)
+                intent. apply {
+                    //typeプロパティで、インテントのタイプを設定
+                    type = "text/plain"
+                    putExtra(Intent.EXTRA_TEXT, text)
+                }
+                //毎回アプリの選択画面を表示させたいときはIntentクラスのクラスメソッドであるcreateChooserを使う
+                val chooser = Intent.createChooser(intent, null)
+                if(intent.resolveActivity(packageManager) != null)
+                    startActivity(chooser)
+
+                return true
+            }
+            R.id.browse -> {
+                val url: String = "http://www.yahoo.co.jp/"
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.data = Uri.parse(url)
+                if (intent.resolveActivity(packageManager) != null){
+                    startActivity(intent)
+                }
+                return true
+            }
+        }
+        return super.onContextItemSelected(item)
     }
 }
